552c2ae483433b2c12f4eefc12c39b8ea3c12f8e
DanBi <danielebelfiore7@gmail.com>
2020-01-08 00:49:25 +0100

    Minor Update

--- post.php
+++ post.php
@@ -3,10 +3,14 @@ include_once 'core/bootstrap.php';
 include_once 'entity/PostRepository.php';
 include_once 'entity/Post.php';
 include 'view/post.php';
-if (isset($_POST['title'])) {
-    $post = new Post($_POST['title'], $_POST['content'], $_SESSION['loggedUser']['name'], explode(", ", $_POST['tags']));
-    $repo->save($post);
-    $repo->uploadDatabase($post);
-    
-    header('Location: index.php');
+if (!isLogged()) {
+    header("Location: index.php");
+} else {
+    if (isset($_POST['title'])) {
+        $post = new Post($_POST['title'], $_POST['content'], $_SESSION['loggedUser']['name'], explode(", ", $_POST['tags']));
+        $repo->save($post);
+        $repo->uploadDatabase($post);
+
+        header('Location: index.php');
+    }
 }
